43ceb66ece283d367fd6087dd879f84a38dbaa99
ketans2141 <105305997+ketans2141@users.noreply.github.com>
2022-10-09 14:34:48 +0530

    Covid-tracker

---
+++ covid-tracker/src/App.js
@@ -0,0 +1,11 @@
+import {FetchCovidData} from "./components/FetchCovidData"
+
+function App() {
+  return (
+    <>
+      <FetchCovidData/>
+    </>
+  );
+}
+
+export default App;
---
+++ covid-tracker/src/components/readme.md
@@ -0,0 +1,2 @@
+<h1>Output</h1>
+<img src="/Images/Output1.png">
